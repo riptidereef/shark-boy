79b6689d5868bb8dc08968f2a669c9d364538113
RiptideReef <jditzel02@gmail.com>
2025-09-22 23:23:58 -0400

    Project setup and organization.

---
+++ build/lib/raylib/raylib/CMakeFiles/raylib.dir/rtext.c.obj.d
@@ -0,0 +1,31 @@
+lib/raylib/raylib/CMakeFiles/raylib.dir/rtext.c.obj: \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\rtext.c \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\raylib.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ C:/msys64/mingw64/include/vadefs.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stdbool.h \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\config.h \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\utils.h \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\rlgl.h \
+ C:/msys64/mingw64/include/stdlib.h C:/msys64/mingw64/include/corecrt.h \
+ C:/msys64/mingw64/include/corecrt_wstdlib.h \
+ C:/msys64/mingw64/include/limits.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/sec_api/stdlib_s.h \
+ C:/msys64/mingw64/include/malloc.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/mm_malloc.h \
+ C:/msys64/mingw64/include/errno.h C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h \
+ C:/msys64/mingw64/include/string.h \
+ C:/msys64/mingw64/include/sec_api/string_s.h \
+ C:/msys64/mingw64/include/ctype.h \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\external/stb_rect_pack.h \
+ C:/msys64/mingw64/include/assert.h C:/msys64/mingw64/include/math.h \
+ C:\Users\jditz\OneDrive\Desktop\C++\ Projects\Projects\shark-boy\lib\raylib\src\external/stb_truetype.h
---
+++ build/lib/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jditz/OneDrive/Desktop/C++ Projects/Projects/shark-boy/lib/raylib/src
+# Build directory: C:/Users/jditz/OneDrive/Desktop/C++ Projects/Projects/shark-boy/build/lib/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
