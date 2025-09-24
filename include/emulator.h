cc8a901d99e5d99377be5c38bfa15f374f5ae9c0
RiptideReef <jditzel02@gmail.com>
2025-09-24 00:11:54 -0400

    Cartridge printing ROM data and emulator class.

--- include/emulator.h
+++ include/emulator.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <string>
+#include "common.h"
 #include "cartridge.h"
 using namespace std;
 
 class Emulator {
 public:
+    bool paused;
+    bool running;
+    u64 ticks;
+
     void loadCartridge(const string& romPath);
 
 private:
