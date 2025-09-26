bff77ccf47f9379d4b8aedbdb5eec7c8f9627688
RiptideReef <jditzel02@gmail.com>
2025-09-25 23:40:28 -0400

    CPU registers added.

--- include/cpu.h
+++ include/cpu.h
@@ -1,5 +1,17 @@
 #pragma once
 
 class CPU {
+public:
+    struct {
+        union { struct { u8 f, a; }; u16 af; };
+        union { struct { u8 c, b; }; u16 bc; };
+        union { struct { u8 e, d; }; u16 de; };
+        union { struct { u8 l, h; }; u16 hl; };
+    } regs;
+
+    u16 sp;
+    u16 pc;
+
+private:
 
 };
